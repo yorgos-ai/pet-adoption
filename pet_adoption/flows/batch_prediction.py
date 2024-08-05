8cc7f2fe21993fbb4ad0c2e2bc3e547b43fc8a2e
Ubuntu <ubuntu@ip-172-31-32-68.eu-west-1.compute.internal>
2024-08-05 12:00:08 +0000

    store prediction df in S3

--- pet_adoption/flows/batch_prediction.py
+++ pet_adoption/flows/batch_prediction.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 
 from dotenv import load_dotenv
 from prefect import flow
@@ -12,6 +13,7 @@ from pet_adoption.flows.tasks import (
     preprocess_data,
     read_data_from_s3,
     save_dict_in_s3,
+    store_data_in_s3,
 )
 
 TARGET = "AdoptionLikelihood"
@@ -46,11 +48,14 @@ def batch_prediction_flow() -> None:
     5. Read the training data from S3 and apply the loaded models to get predictions.
     6. Create monitoring metrics for the test set based on the train set.
     """
+    today = date.today()
+    batch_date = today.strftime("%Y-%m-%d")
+
     # read test data from S3
-    df = read_data_from_s3(bucket_name=os.getenv("S3_BUCKET"), file_key="data/df_test.csv")
+    df_test = read_data_from_s3(bucket_name=os.getenv("S3_BUCKET"), file_key="data/df_test.csv")
 
     # preprocess the data
-    df = preprocess_data(df)
+    df_test = preprocess_data(df_test)
 
     # get the MLflow run id of the production model
     RUN_ID = get_production_model_run_id(bucket_name=os.getenv("S3_BUCKET_MLFLOW"), file_key="production_model.json")
@@ -60,17 +65,25 @@ def batch_prediction_flow() -> None:
     model = load_model(run_id=RUN_ID)
 
     # make batch predictions
-    df = apply_model(df, model)
+    df_test = apply_model(df_test, model)
+
+    # store the test data in S3
+    store_data_in_s3(
+        df=df_test,
+        bucket_name=os.getenv("S3_BUCKET"),
+        file_key=f"predictions/{batch_date.replace('-', '_')}_df_test.csv",
+    )
 
     # read the training data from S3 for monitoring
     df_train = read_data_from_s3(bucket_name=os.getenv("S3_BUCKET"), file_key="data/df_train.csv")
     df_train = apply_model(df_train, model)
 
     # Evidently batch prediction monitoring metrics
-    metrics_dict = monitor_model_performance(reference_data=df_train, current_data=df)
+    metrics_dict = monitor_model_performance(reference_data=df_train, current_data=df_test)
     save_dict_in_s3(metrics_dict, os.getenv("S3_BUCKET"), "data/monitoring_metrics_prediction.json")
-    extract_report_data(batch_date="2024-08-03", metrics_dict=metrics_dict, db_name="predict_monitoring")
-    return df
+    extract_report_data(batch_date=batch_date, metrics_dict=metrics_dict, db_name="predict_monitoring")
+
+    return df_test
 
 
 if __name__ == "__main__":
